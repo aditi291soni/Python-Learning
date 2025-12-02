97b08a7d95dc933d46113e1a062af96568ba7c19
aditi291soni <aditi291soni@gmail.com>
2025-12-02 23:14:20 +0530

    Even or Odd Checker

--- Day1/calculator.py
+++ Day1/calculator.py
@@ -1,7 +1,6 @@
-A=input("Input first no ")  
-B=input("Input first no ") 
+A=input("Input  no ")  
+if int(A) % 2 == 0:
+    print('Even')
+else:
+    print('Odd')
 
-print("Before: A= ",int(A) ," B= ",int(B))
-
-A, B = B, A
-print("After: A= ",int(A) ," B= ",int(B))
